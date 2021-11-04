80430c4e311c27af20b6f5726aa90042a88e18a2
phin <seongyeon.kang@solarconnect.kr>
2021-11-04 23:21:55 +0900

    2021-11-04 JS

--- JS/leet_20.js
+++ JS/leet_20.js
@@ -14,10 +14,15 @@
  * (O) arr.length === 0
  * (X) arr === []
  * []은 js에서 false가 아님
+ *
+ * Object로 만들어둔 dictionary에 접근할 때
+ * (O) dict[key]
+ * (X) dict(key)
  */
 
 // 방법 1
 // Map
+// 여는 괄호를 array에 담아두며 돎
 var isValid = function(s) {
     let brackets = new Map().set('(', ')').set('[', ']').set('{', '}');
     // let brackets = new Map([['(', ')'], ['[', ']'], ['{', '}']]);
@@ -33,4 +38,30 @@ var isValid = function(s) {
     }
 
     return opens.length === 0 ? true : false;
+};
+
+
+// 방법 2
+// Object
+// 닫는 괄호를 array에 담아두며 돎
+var isValid = function(s) {
+    if (s.length % 2 === 1) {
+        return false;
+    }
+
+    let brackets = {')' : '(', ']' : '[', '}' : '{'};
+    let remains = [];
+
+    for (i=0; i < s.length; i++) {
+        if (brackets[s[i]]) {
+            if (brackets[s[i]] !== remains[remains.length-1]) {
+                return false;
+            } else {
+                remains.pop();
+            }
+        } else {
+            remains.push(s[i]);
+        }
+    }
+    return !remains.length;
 };
---
+++ JS/leet_21.js
@@ -0,0 +1,8 @@
+/* https://leetcode.com/problems/merge-two-sorted-lists/
+ *
+ * linked list는 array가 아니라서 concat, spread operator(...)를 사용할 수 없음
+ * (X) lst1.concat(lst2) -> concat not a function
+ * (X) [...lst1, ...lst2] -> object is not iterable
+ */
+
+// unsolved
